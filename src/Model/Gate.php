fdd49c5e2470bb1e10768125f2646c6b6437abd7
Edgar Zhiznevski <edgarz@advansys.com>
2023-12-21 12:58:09 +0300

    Изменение адресе сервера 1С

--- src/Model/Gate.php
+++ src/Model/Gate.php
@@ -8,35 +8,35 @@ class Gate extends Model
 {
     public function getUidByPhone($phone)
     {
-        $result = $this->auth("Site_Exchange", "dS-89eoeX_")->get("https://1c.corp-pobeda63.com:4433/uf_static_test/hs/API/Partner?phone=" . $phone);
+        $result = $this->auth("Site_Exchange", "dS-89eoeX_")->get("https://1c.pobeda-corp.com:4433/uf_static_test/hs/API/Partner?phone=" . $phone);
 
         return (Error::is()) ? ["ЕстьОшибка" => true, "ТекстОшибки" => $this->errors()] : $result->getJsonResult();
     }
 
     public function getListByUid($uid)
     {
-        $result = $this->auth("Site_Exchange", "dS-89eoeX_")->get("https://1c.corp-pobeda63.com:4433/uf_static_test/hs/API/PartnerAgreements?UID=" . $uid . "&type_of_contract=Залог&status=Активен");
+        $result = $this->auth("Site_Exchange", "dS-89eoeX_")->get("https://1c.pobeda-corp.com:4433/uf_static_test/hs/API/PartnerAgreements?UID=" . $uid . "&type_of_contract=Залог&status=Активен");
 
         return (Error::is()) ? false : $result->getJsonResult();
     }
 
     public function getContractHistory($contractUID)
     {
-        $result = $this->auth("Site_Exchange", "dS-89eoeX_")->get("https://1c.corp-pobeda63.com:4433/uf_static_test/hs/API/PartnerAgreementHistory?UID=" . $contractUID);
+        $result = $this->auth("Site_Exchange", "dS-89eoeX_")->get("https://1c.pobeda-corp.com:4433/uf_static_test/hs/API/PartnerAgreementHistory?UID=" . $contractUID);
 
         return (Error::is()) ? ["ЕстьОшибка" => true, "ТекстОшибки" => $this->errors()] : $result->getJsonResult();
     }
 
     public function getProlongationSumm($params)
     {
-        $result = $this->auth("WEBService", "dS-89eoeX_")->get("https://1c.corp-pobeda63.com:5446/uf_product_test_services/hs/Prolongation/GetProlongationSum?date=" . $params["date"] . "&number=" . $params["number"] ."&date_of_calc=" . $params["date_of_calc"] . "&UID=" . $params["UID"]);
+        $result = $this->auth("WEBService", "dS-89eoeX_")->get("https://1c.pobeda-corp.com:5446/uf_product_test_services/hs/Prolongation/GetProlongationSum?date=" . $params["date"] . "&number=" . $params["number"] ."&date_of_calc=" . $params["date_of_calc"] . "&UID=" . $params["UID"]);
 
         return (Error::is()) ? ["ЕстьОшибка" => true, "ТекстОшибки" => $this->errors()] : $result->getJsonResult();
     }
 
     public function prolongateContract($params)
     {
-        $result = $this->auth("WEBService", "dS-89eoeX_")->post("https://1c.corp-pobeda63.com:5446/uf_product_test_services/hs/Prolongation/AgreementProlongation?UID=" .$params["UID"] ."&date=" . $params["date"] . "&date_of_calc=" . $params["date_of_calc"] . "&number=" . $params["number"] . "&check_number_KKM=" . $params["check_number_KKM"] . "&ogrn_number=" . $params["ogrn_number"] . "&prolongation_sum=" . $params["prolongation_sum"]);
+        $result = $this->auth("WEBService", "dS-89eoeX_")->post("https://1c.pobeda-corp.com:5446/uf_product_test_services/hs/Prolongation/AgreementProlongation?UID=" .$params["UID"] ."&date=" . $params["date"] . "&date_of_calc=" . $params["date_of_calc"] . "&number=" . $params["number"] . "&check_number_KKM=" . $params["check_number_KKM"] . "&ogrn_number=" . $params["ogrn_number"] . "&prolongation_sum=" . $params["prolongation_sum"]);
 
         return (Error::is()) ? ["ЕстьОшибка" => true, "ТекстОшибки" => $this->errors()] : $result->getJsonResult();
     }
